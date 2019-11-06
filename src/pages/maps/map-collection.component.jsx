4aeb4c72d3543a0206c1c425f62363918627988a
Kenny Downs <kdowns@repairpal.com>
2019-11-05 17:45:45 -0800

    revert to working front end

--- src/pages/maps/map-collection.component.jsx
+++ src/pages/maps/map-collection.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import MapCollectionItem from '../../components/map-collection-item/map-collection-item.component';
+// import MapCollectionItem from '../../components/map-collection-item/map-collection-item.component';
 
-import { selectCollection } from '../../redux/section/section.selectors';
+// import { selectCollection } from '../../redux/section/section.selectors';
 
 import {
   MapCollectionPageContainer,
